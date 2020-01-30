9825df74444c4e7a80389425051edcf23062b44c
蒋文科 <839928622@qq.com>
2020-01-30 14:24:32 +0800

    DatabaseHelper数据库助手封装，增加插入，更新，删除的封装

---
+++ NotesApp/ViewModel/DatabaseHelper.cs
@@ -0,0 +1,63 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp.ViewModel
+{
+ public   class DatabaseHelper
+    {
+        private static  string dbFile = Path.Combine(Environment.CurrentDirectory, "notedb.db");
+
+        public static bool Insert<T> (T item)
+        {
+            bool result = false;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(dbFile))
+            {
+                sQLiteConnection.CreateTable<T>();
+              int effectedOfRow =  sQLiteConnection.Insert(item);
+                if (effectedOfRow > 0)
+                {
+                result = true;
+
+                }
+            }
+                return result; 
+        }
+
+        public static bool Update<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(dbFile))
+            {
+                sQLiteConnection.CreateTable<T>();
+                int effectedOfRow =   sQLiteConnection.Update(item);
+                if (effectedOfRow > 0)
+                {
+                    result = true;
+
+                }
+            }
+            return result;
+        }
+
+        public static bool Delete<T>(T item)
+        {
+            bool result = false;
+            using (SQLiteConnection sQLiteConnection = new SQLiteConnection(dbFile))
+            {
+                sQLiteConnection.CreateTable<T>();
+                int effectedOfRow = sQLiteConnection.Delete(item);
+                if (effectedOfRow > 0)
+                {
+                    result = true;
+
+                }
+            }
+            return result;
+        }
+    }
+}
